7a23004b0933e9a57a3649a37a777865245d03b1
Robert Holfeld <robert.holfeld@plenigo.com>
2023-04-04 11:10:16 +0200

    - fixed clear prepare multi line text box

--- src/main/java/com/plenigo/pdflayout/element/text/AbstractPLMultiLineTextBox.java
+++ src/main/java/com/plenigo/pdflayout/element/text/AbstractPLMultiLineTextBox.java
@@ -291,6 +291,9 @@ public abstract class AbstractPLMultiLineTextBox<IMPLTYPE extends AbstractPLMult
             }
         }
 
+        // clean the text list
+        m_aTextList.removeAll();
+
         if (multiLineTextBox != null) {
             onPrepareMultiLineTextBox(aCtx, multiLineTextBox, fMaxFontHeight);
 
